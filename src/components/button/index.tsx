dd4280662be5eef1d2fb31428e3584e33bc3ca1b
WendelRodriguesz <wendeldev2010@gmail.com>
2024-08-28 21:05:05 -0300

    configuração inicial, button teste

---
+++ src/components/button/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./button.css";
+
+interface Props {
+    children?: React.ReactNode
+    type?: "button" | "submit" | "reset" | undefined
+    onClick?: () => void
+}
+
+export default function Button({ type, onClick, children }: Props) {
+    return (
+        <button 
+            type={type}
+            className="button"
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+}
